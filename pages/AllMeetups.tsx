b0a3b83a9ec8c96d93acae14d790fefca58fc40b
atorau <hfardura@hotmail.com>
2022-11-23 13:37:50 +0100

    get list from firebase

--- pages/AllMeetups.tsx
+++ pages/AllMeetups.tsx
@@ -2,8 +2,6 @@ import React = require('react');
 import MeetupList from '../components/meetups/MeetupList';
 import { Meetup } from '../shared/interfaces';
 
-
-
 const DUMMY_DATA: Meetup[] = [
   {
     id: 'm1',
@@ -26,10 +24,45 @@ const DUMMY_DATA: Meetup[] = [
 ];
 
 const AllMeetupsPage = (): React.ReactElement => {
-  return <section>
-    <h1>All Meetups</h1>
-    <MeetupList meetups={DUMMY_DATA}/>
-    </section>;
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [loadedMeetups, setLoadedMeetups] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch(
+      'https://react-getting-started-dae49-default-rtdb.firebaseio.com/meetups.json'
+    ).then((respone) => {
+      return respone.json();
+    }).then((data:Meetup[]) =>{
+      const meetups: Meetup[] = [];
+
+      for ( const key in data) {
+        const meetup: Meetup = {
+          id: key,
+          ...data[key]
+        }
+        meetups.push(meetup)
+      }
+
+      setIsLoading(false);
+      setLoadedMeetups(meetups)
+    });
+  }, [])
+  
+
+  if (isLoading) {
+    return (
+      <section>
+        <p>Loading...</p>
+      </section>
+    )
+  }
+
+  return (
+    <section>
+      <h1>All Meetups</h1>
+      <MeetupList meetups={loadedMeetups} />
+    </section>
+  );
 };
 
 export default AllMeetupsPage;
